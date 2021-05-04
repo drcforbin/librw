6e503619a1fd3d2b2cb22efbb576bd6760c45bac
drcforbin <ryan@c.oloss.us>
2021-05-04 12:35:52 -0500

    moved utf8 to librw

--- bench/main.cpp
+++ bench/main.cpp
@@ -2,18 +2,25 @@
 #include "nanobench.h"
 
 #include "rw/logging.h"
+#include "rw/utf8.h"
+
+#define LOGGER() (rw::logging::get("librw-bench"))
 
 // todo: better way than extern.
 
 extern void bench_map_persistent(ankerl::nanobench::Config& cfg);
 extern void bench_map_transient(ankerl::nanobench::Config& cfg);
+extern void bench_utf8_encoding(ankerl::nanobench::Config& cfg);
+extern void bench_utf8_decoding(ankerl::nanobench::Config& cfg);
 
 int main()
 {
-    rw::logging::get("env")->level(rw::logging::log_level::off);
+    rw::utf8::set_locale();
 
     auto cfg = ankerl::nanobench::Config();
 
     bench_map_persistent(cfg);
     bench_map_transient(cfg);
+    bench_utf8_encoding(cfg);
+    bench_utf8_decoding(cfg);
 }
